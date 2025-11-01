d69e92733db0fcf2112774158661da9b5f33ba71
CALIXTICO <78048582+CALIXTICO@users.noreply.github.com>
2025-11-01 03:41:07 -0500

    Revert "Feature/home avg ratings"

--- app/src/main/java/com/example/kotlinview/data/user/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinview.data.user
-
-import com.example.kotlinview.model.User
-
-interface UserRepository {
-    suspend fun getByEmail(email: String): User?
-    suspend fun setLastSignInNow(email: String)
-}
